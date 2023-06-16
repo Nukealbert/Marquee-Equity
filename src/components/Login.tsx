082664a82b0977afdf880729fa25557bf2f4de34
Kundan Kumar <39196236+Nukealbert@users.noreply.github.com>
2023-06-16 07:55:47 +0530

    Login is also done

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import './styles.css'
 
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError]=useState('');
   const navigate = useNavigate();
 
     const handleLogin=()=>{
@@ -13,22 +15,34 @@ const Login = () => {
         localStorage.setItem('isLoggedIn', 'true');
         navigate('/dashboard');
       }
+      else{
+          setError("Try Again ");
+          setTimeout(() => {
+            setError('');
+          }, 3000);
+      }
     }
   return (
-    <div>
+    <div className='login_h'>
+      <span className="heading">Login to Tasker</span>
       <input
+        className='input_login'
         type="text"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
         placeholder="Username"
+        required
       />
       <input
+        className='input_login'
         type="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
+        required
       />
-      <button onClick={handleLogin}>Login</button>
+      <p style={{color:'red', fontSize:'25px', fontWeight:'bold'}}>{error}</p>
+      <button className='login_submit' onClick={handleLogin}>Login</button>
     </div>
   )
 }
